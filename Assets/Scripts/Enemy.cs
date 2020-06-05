62b74958e555ebc643a2e1282f3c8088fff8bde4
Rafael Braga <rafael.m.braga@hotmail.com>
2020-06-06 01:37:31 +0200

    Adding enemies, laser shooting, and a lot of changes.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // mover 4mts/s pra baixo
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        // se chegar no final da tela, renascer no topo da tela com uma nova posição randomica na posição x
+        if (transform.position.y <= -5.38f)
+        {
+            transform.position = new Vector3(x: Random.Range(-9.45f, 9.45f), y: 7f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,44 +7,58 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed = 3.5f;
 
+    [SerializeField]
+    private GameObject _laserPrefab;
+
+    [SerializeField]
+    private float _fireRate = 0.15f;
+    private float _canFire = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        // representa a posição inicial do player, dada a posição 
-        //x = lateral, y = altura, z = profundidade
         transform.position = new Vector3(x: 0, y: 0, z: 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+    }
+
+    void CalculateMovement()
+    {
         // Usando a classe Input, conseguimos mapear as teclas que queremos usar no jogo
-        // Observar na Unity > Edit > Project Settings > Input Manager > Axes...
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         // Aplicando deltaTime, é como se estivessemos nos movendo 1 metro
         // por segundo, dada a nossa velocidade...
-        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
         transform.Translate(new Vector3(x: horizontalInput, y: verticalInput, z: 0) * _speed * Time.deltaTime);
 
-        // Se a posição do player na posição y for maior que 0 > setar y = 0
-        if (transform.position.y >= 0)
-        {
-            transform.position = new Vector3(x: transform.position.x, y: 0);
-        }
-        else if (transform.position.y <= -3.97f)
-        {
-            transform.position = new Vector3(x: transform.position.x, y: -3.97f);
-        }
+        // limita o player entre a posição -3.97 e 0, utilizando a função Clamp da Mathf
+        transform.position = new Vector3(x: transform.position.x, y: Mathf.Clamp(transform.position.y, -3.97f, 0));
 
-        if(transform.position.x <= -11.28f)
+        if (transform.position.x <= -11.28f)
         {
             transform.position = new Vector3(x: 11.15f, y: transform.position.y);
-        } else if(transform.position.x >= 11.15f)
+        }
+        else if (transform.position.x >= 11.15f)
         {
             transform.position = new Vector3(x: -11.28f, y: transform.position.y);
         }
+
+        transform.position = new Vector3(x: Mathf.Clamp(transform.position.x, -11.28f, 11.15f), y: transform.position.y);
+    }
+
+    void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + (_laserPrefab.transform.up * 0.8f), Quaternion.identity);
     }
 }
