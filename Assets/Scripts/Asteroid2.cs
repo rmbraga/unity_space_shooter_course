ccb2d9440647e3bd3a9f64da46458d5be53b5f44
Rafael Braga <rafael.m.braga@hotmail.com>
2020-06-11 09:38:53 +0200

    Aqui eu implementei o observer design pattern pra gerenciar audio!

---
+++ Assets/Scripts/Asteroid2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid2 : MonoBehaviour
+{
+
+    [SerializeField] private float _rotationSpeed = 19f;
+    private Animator _animator;
+    private int _shotsToDestroy = 0;
+    private SpawnManager _spawnManager;
+
+    public static event Action OnAsteroidDestroy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+
+        Utils.CheckIfGameObjectIsNull(_animator);
+        Utils.CheckIfGameObjectIsNull(_spawnManager);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * _rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+            _shotsToDestroy++;
+            Destroy(other.gameObject);
+
+            if (_shotsToDestroy == 3)
+            {
+                _animator.SetTrigger("OnEnemyDeath");
+                Destroy(this.gameObject, 2.3f);
+                OnAsteroidDestroy?.Invoke();
+                _spawnManager.StartSpawning();
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/AudioSourceTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceTest : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private void Awake() => _audioSource = GetComponent<AudioSource>();
+
+    private void OnEnable() => Asteroid2.OnAsteroidDestroy += PlaySound;
+
+    private void OnDisable() => Asteroid2.OnAsteroidDestroy -= PlaySound;
+
+    private void PlaySound() => _audioSource.Play();
+}
